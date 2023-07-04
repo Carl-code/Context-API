fb6005df02d5dcc72e4bc216510cd65512aedb57
Carl-code <arimatea865@gmail.com>
2023-07-04 10:37:49 -0600

    Ejemplos de useEffect

--- src/components/container/task.list.jsx
+++ src/components/container/task.list.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TaskComponent from "../pure/task";
 import { Task } from "../../models/task.class";
 import { LEVELS } from "../../models/levels.enum";
 
 const TaskListComponent = () => {
+
   const defaultTask = new Task(
     "Example",
     "Default Description",
     false,
     LEVELS.NORMAL
   );
+
+  // Estado del Componente
+  const [task, setTask] = useState([defaultTask]);
+
+  // Control del Ciclo de vida del Componente
+  useEffect(() => {
+    console.log("Task State has been Modified");
+    return () => {
+    console.log("TaskList component is going to unmount...");
+    }
+  }, [task])
+  
+
+  const changeCompleted = (id) => {
+    console.log("");
+  }
 
   return (
     <div>
